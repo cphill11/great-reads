c8d70a43ea159715be7c11cc5b012eb637e05d38
Criste Phillips <criste.phillips@yahoo.com>
2022-04-23 17:24:03 -0400

    initial placeholder images added

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import WebsiteContainer from "./components/WebsiteContainer";
+
+const App = () => <WebsiteContainer />;
+
+
+// setting up function to use react in future
+
+// function App () {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+export default App;
---
+++ client/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../index.css';
+
+function MainPage() {
+    return (
+        <section>
+            This is the MainPage's placeholder.
+        </section>
+    );
+}
+
+export default MainPage;
