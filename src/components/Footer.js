62409b6f27ab2c1f19fd1b489a0216ee63567753
DCT <mirachan2332@gmail.com>
2022-04-23 11:55:47 -0500

    set up components folder with barebone framework for website

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../index.css';
+
+function Footer() {
+    return (
+        <footer>
+            This is a footer placeholder.
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../index.css';
+
+function Header(props) {
+    return (
+        <header>
+            This is a header placeholder.
+        </header>
+    );
+}
+export default Header;
