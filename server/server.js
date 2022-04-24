629239f4da7fbb4bb4e06d5912f9a45afc5e4af5
Criste Phillips <criste.phillips@yahoo.com>
2022-04-23 21:48:23 -0400

    server js template added

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// implement Apollo Server; apply it to Express server as middleware
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+const db = require('./config/connection');
+// const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
