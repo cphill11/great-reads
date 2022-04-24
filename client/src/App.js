e7d1274bd9af6c1e4054e13b08b34f9ee06572a4
Criste Phillips <criste.phillips@yahoo.com>
2022-04-23 20:04:14 -0400

    redacted code re-added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,60 @@
 import React from 'react';
 
-import WebsiteContainer from "./components/WebsiteContainer";
+// import PortfolioContainer from "./components/Portfolio-container";
 
-const App = () => <WebsiteContainer />;
+// const App = () =>  <PortfolioContainer />;
 
 
-// setting up function to use react in future
 
-// function App () {
-//   return (
-//     <div>
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
 
-//     </div>
-//   )
-// }
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+    </ApolloProvider>
+  );
+}
 
 export default App;
