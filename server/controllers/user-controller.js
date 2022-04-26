998b0cbf5adb17bea2557c9883505823bd130e68
DCT <mirachan2332@gmail.com>
2022-04-26 13:36:27 -0500

    route updates

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -5,8 +5,59 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 module.exports = {
-  // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
+  // get all users
+  async getAllUser(req, res) {
+      User.find({})
+        // .populate({
+        //   path: 'thoughts',
+        //   select: '-__v'
+        // })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+      },
+
+  // get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      // .populate({
+      //   path: 'thoughts',
+      //   select: '-__v'
+      // })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+  
+    // delete user
+    deleteUser({ params }, res) {
+      User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+
+  // get a single user by either their id or their username <--- intending to be to get OWN user
+  async getMyUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
     });
@@ -17,6 +68,7 @@ module.exports = {
 
     res.json(foundUser);
   },
+
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
   async createUser({ body }, res) {
     const user = await User.create(body);
@@ -27,6 +79,7 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
+
   // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
   // {body} is destructured req.body
   async login({ body }, res) {
@@ -43,6 +96,7 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
+
   // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
   async saveBook({ user, body }, res) {
@@ -59,6 +113,7 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
+  
   // remove a book from `savedBooks`
   async deleteBook({ user, params }, res) {
     const updatedUser = await User.findOneAndUpdate(
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,46 @@
 const router = require('express').Router();
 const {
+  getAllUser,
+  getUserById,
   createUser,
-  getSingleUser,
+  getMyUser,
+  updateUser,
+  deleteUser,
   saveBook,
   deleteBook,
-  login,
+  login
 } = require('../../controllers/user-controller');
 
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
+// /api/users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser).put(authMiddleware, saveBook);
+
+// api/users/:id
+router
+  .route('/:id')
+  .get(authMiddleware, getMyUser)
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:id/login')
+  .post(login);
+
+router
+  .route('/books/bookId')
+  .delete(authMiddleware, deleteBook);
+
+
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveBook);
-
-router.route('/login').post(login);
-
-router.route('/me').get(authMiddleware, getSingleUser);
-
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+// router.route('/').post(createUser).put(authMiddleware, saveBook);
+// router.route('/login').post(login);
+// router.route('/me').get(authMiddleware, getMyUser);
+// router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
 module.exports = router;
